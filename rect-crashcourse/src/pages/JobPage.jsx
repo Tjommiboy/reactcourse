d5e7ef337e2c46999547f551c203b7a33e4180ae
Tjommiboy <119463874+Tjommiboy@users.noreply.github.com>
2025-02-23 04:05:46 +0700

    fixed server issue

--- rect-crashcourse/src/pages/JobPage.jsx
+++ rect-crashcourse/src/pages/JobPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams, useLoaderData } from "react-router-dom";
-import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
 // import { useState, useEffect } from "react";
 // import Spinner from "../components/Spinner";
@@ -46,12 +46,10 @@ const JobPage = () => {
             <main>
               <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
                 <div className="text-gray-500 mb-4">{job.type}</div>
-                <h1 className="text-3xl font-bold mb-4">
-                  Senior React Developer
-                </h1>
+                <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
                 <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-                  <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
-                  <p className="text-orange-700">Boston, MA</p>
+                  <FaMapMarker className="text-orange-700 inline text-lg mb-1 mr-1" />
+                  <p className="text-orange-700">{job.location}</p>
                 </div>
               </div>
 
