1cd002d26380853c772941f2bcc2c1085a69c754
Tjommiboy <119463874+Tjommiboy@users.noreply.github.com>
2025-02-25 13:54:17 +0700

    adding mobil version nav

--- rect-crashcourse/src/components/NavBar.jsx
+++ rect-crashcourse/src/components/NavBar.jsx
@@ -10,33 +10,35 @@ const NavBar = () => {
       : "text-white hover:bg-purple-600 hover:animate-pulse hover:text-yellow-300 hover:font-bold rounded-md px-3 py-2";
 
   return (
-    <nav className="bg-indigo-700 border-b border-indigo-500">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex h-20 items-center justify-between">
-          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
-              <img className="h-10 w-auto" src={logo} alt="React Jobs" />
-              <span className="hidden md:block  text-yellow-400  text-2xl font-bold ml-2 hover:shadow-2xl hover:text-yellow-300  ">
-                Gimmi Bid n´Sell
-              </span>
-            </NavLink>
-            <div className="md:ml-auto">
-              <div className="flex space-x-2">
-                <NavLink to="/" className={linkClass}>
-                  Home
-                </NavLink>
-                <NavLink to="/jobs" className={linkClass}>
-                  Jobs
-                </NavLink>
-                <NavLink to="/add-job" className={linkClass}>
-                  Add Job
-                </NavLink>
+    <>
+      <nav className="bg-indigo-700 border-b border-indigo-500">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="flex h-20 items-center justify-between">
+            <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+              <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
+                <img className="h-10 w-auto" src={logo} alt="React Jobs" />
+                <span className="hidden md:block  text-yellow-400  text-2xl font-bold ml-2 hover:shadow-2xl hover:text-yellow-300  ">
+                  Gimmi Bid n´Sell
+                </span>
+              </NavLink>
+              <div className="md:ml-auto">
+                <div className="flex space-x-2">
+                  <NavLink to="/" className={linkClass}>
+                    Home
+                  </NavLink>
+                  <NavLink to="/jobs" className={linkClass}>
+                    Jobs
+                  </NavLink>
+                  <NavLink to="/add-job" className={linkClass}>
+                    Add Job
+                  </NavLink>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
