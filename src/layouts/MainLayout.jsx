8108296402557ab4dba26f593ef6abc5c9813823
Tjommiboy <119463874+Tjommiboy@users.noreply.github.com>
2025-06-17 23:36:42 +0700

    uppercasing fixed

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Navbar from "../components/Navbar";
+import NavBar from "../components/NavBar";
 
 const MainLayout = () => {
   return (
     <>
-      <Navbar />
+      <NavBar />
       <Outlet />
       <ToastContainer />
     </>
