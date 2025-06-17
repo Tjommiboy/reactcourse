2770213e6252bde9ee41bb08c1f479ae6c38fa5f
Tjommiboy <119463874+Tjommiboy@users.noreply.github.com>
2025-06-17 23:40:18 +0700

    uppercasing for JobListing

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import React from "react";
-import JobListing from "./jobListing";
+import JobListing from "./JobListing";
 import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false }) => {
